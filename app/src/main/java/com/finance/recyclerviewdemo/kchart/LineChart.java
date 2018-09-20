703ab036371f725fad2f6e088dcecd3e469a5c24
JackieLin <feifei5292190@163.com>
2018-09-20 15:48:26 +0800

    折线图移动

--- app/src/main/java/com/finance/recyclerviewdemo/kchart/LineChart.java
+++ app/src/main/java/com/finance/recyclerviewdemo/kchart/LineChart.java
@@ -4,11 +4,25 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.RectF;
 import android.support.annotation.Nullable;
+import android.support.v4.view.MotionEventCompat;
+import android.support.v4.view.VelocityTrackerCompat;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.widget.ScrollerCompat;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.util.TypedValue;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
 import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.animation.Interpolator;
+import android.widget.Scroller;
 
 import com.finance.recyclerviewdemo.kchart.bean.NewEntryData;
+
+import static android.support.v4.view.ViewCompat.TYPE_NON_TOUCH;
+import static android.support.v4.view.ViewCompat.TYPE_TOUCH;
+import static android.support.v4.widget.ViewDragHelper.INVALID_POINTER;
 
 /**
  * Created by Jackie on 2018/9/19.
@@ -18,7 +32,32 @@ public class LineChart extends View {
     private LineRender lineRender;
     private float contentLeftOffset;
     private NewEntryData mData;
+    private VelocityTracker mVelocityTracker;
+    private int mMinFlingVelocity;
+    private int mMaxFlingVelocity;
+    private int mTouchSlop;
+    private int mScrollPointerId = INVALID_POINTER;
+    private int mLastTouchX;
+    private int mLastTouchY;
+    private ViewFlinger mViewFlinger = new ViewFlinger();
+    /**
+     * The RecyclerView is not currently scrolling.
+     * @see #getScrollState()
+     */
+    public static final int SCROLL_STATE_IDLE = 0;
 
+    /**
+     * The RecyclerView is currently being dragged by outside input such as user touch input.
+     * @see #getScrollState()
+     */
+    public static final int SCROLL_STATE_DRAGGING = 1;
+
+    /**
+     * The RecyclerView is currently animating to a final position while not under
+     * outside control.
+     * @see #getScrollState()
+     */
+    public static final int SCROLL_STATE_SETTLING = 2;
     public LineChart(Context context) {
         this(context,null);
     }
@@ -29,6 +68,11 @@ public class LineChart extends View {
 
     public LineChart(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        ViewConfiguration vc = ViewConfiguration.get(this.getContext());
+        mTouchSlop = vc.getScaledTouchSlop();
+        mMaxFlingVelocity = vc.getScaledMaximumFlingVelocity();
+        mMinFlingVelocity = vc.getScaledMinimumFlingVelocity();
+
 
         contentRect = new RectF();
         lineRender = new LineRender();
@@ -39,7 +83,7 @@ public class LineChart extends View {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        contentRect.set(contentLeftOffset * 2,contentLeftOffset,w - contentLeftOffset,h - contentLeftOffset);
+        contentRect.set(contentLeftOffset ,contentLeftOffset,w - contentLeftOffset,h - contentLeftOffset*1.5f);
         notifyDataSetChanged(false);
     }
 
@@ -48,6 +92,107 @@ public class LineChart extends View {
         super.onDraw(canvas);
         lineRender.render(canvas);
 
+    }
+
+    private static final String TAG = "LineChart";
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+        if (mVelocityTracker == null) {
+            mVelocityTracker = VelocityTracker.obtain();
+        }
+        boolean eventAddedToVelocityTracker = false;
+        final MotionEvent vtev = MotionEvent.obtain(e);
+        final int action = MotionEventCompat.getActionMasked(e);
+        final int actionIndex = MotionEventCompat.getActionIndex(e);
+        switch (action){
+            case MotionEvent.ACTION_DOWN:
+                mScrollPointerId = e.getPointerId(0);
+                mLastTouchX = (int) (e.getX() + 0.5f);
+                mLastTouchY = (int) (e.getY() + 0.5f);
+                if (mScrollState == SCROLL_STATE_SETTLING) {
+                    setScrollState(SCROLL_STATE_DRAGGING);
+                }
+                Log.i(TAG, "onTouchEvent: ------down--------");
+                break;
+            case MotionEventCompat.ACTION_POINTER_DOWN: {
+                mScrollPointerId = MotionEventCompat.getPointerId(e, actionIndex);
+                mLastTouchX = (int) (MotionEventCompat.getX(e, actionIndex) + 0.5f);
+                mLastTouchY = (int) (MotionEventCompat.getY(e, actionIndex) + 0.5f);
+                break;
+            }
+            case MotionEvent.ACTION_MOVE:
+                final int index = e.findPointerIndex(mScrollPointerId);
+                final int x = (int) (e.getX(index) + 0.5f);
+                final int y = (int) (e.getY(index) + 0.5f);
+                int dx = mLastTouchX - x;
+                int dy = mLastTouchY - y;
+                if (mScrollState != SCROLL_STATE_DRAGGING) {
+                    boolean startScroll = false;
+                    if (Math.abs(dx) > mTouchSlop) {
+                        if (dx > 0) {
+                            dx -= mTouchSlop;
+                        } else {
+                            dx += mTouchSlop;
+                        }
+                        startScroll = true;
+                    }
+                    if (Math.abs(dy) > mTouchSlop) {
+                        if (dy > 0) {
+                            dy -= mTouchSlop;
+                        } else {
+                            dy += mTouchSlop;
+                        }
+                        startScroll = true;
+                    }
+                    if (startScroll) {
+                        setScrollState(SCROLL_STATE_DRAGGING);
+                    }
+                }
+
+                if (mScrollState == SCROLL_STATE_DRAGGING) {
+                    mLastTouchX = x;
+                    mLastTouchY = y;
+                    scroll(dx,0);
+                    Log.i(TAG, "onTouchEvent: ----------move---------------");
+                }
+                break;
+            case MotionEventCompat.ACTION_POINTER_UP: {
+                final int actionIndexDD = e.getActionIndex();
+                if (e.getPointerId(actionIndexDD) == mScrollPointerId) {
+                    // Pick a new pointer to pick up the slack.
+                    final int newIndex = actionIndexDD == 0 ? 1 : 0;
+                    mScrollPointerId = e.getPointerId(newIndex);
+                    mLastTouchX = (int) (e.getX(newIndex) + 0.5f);
+                    mLastTouchY = (int) (e.getY(newIndex) + 0.5f);
+                }
+                break;
+            }
+            case MotionEvent.ACTION_UP: {
+                mVelocityTracker.addMovement(vtev);
+                eventAddedToVelocityTracker = true;
+                mVelocityTracker.computeCurrentVelocity(1000, mMaxFlingVelocity);
+                final float xvel =
+                        -VelocityTrackerCompat.getXVelocity(mVelocityTracker, mScrollPointerId);
+                final float yvel =
+                        -VelocityTrackerCompat.getYVelocity(mVelocityTracker, mScrollPointerId);
+                if (!((xvel != 0 || yvel != 0) && fling((int) xvel, (int) yvel))) {
+                    setScrollState(SCROLL_STATE_IDLE);
+                }
+                resetTouch();
+                Log.i(TAG, "onTouchEvent: ---------------up-------");
+            }
+            break;
+            case MotionEvent.ACTION_CANCEL:
+                resetTouch();
+                setScrollState(SCROLL_STATE_IDLE);
+                break;
+        }
+        if (!eventAddedToVelocityTracker) {
+            mVelocityTracker.addMovement(vtev);
+        }
+        vtev.recycle();
+
+        return true;
     }
 
     public void setmData(NewEntryData mData) {
@@ -61,5 +206,105 @@ public class LineChart extends View {
         if (invalitate){
             invalidate();
         }
+    }
+    private class ViewFlinger implements Runnable{
+        private int mLastFlingX;
+        private int mLastFlingY;
+        private ScrollerCompat mScroller;
+        final Interpolator sQuinticInterpolator = new Interpolator() {
+            @Override
+            public float getInterpolation(float t) {
+                t -= 1.0f;
+                return t * t * t * t * t + 1.0f;
+            }
+        };
+        public ViewFlinger(){
+            mScroller = ScrollerCompat.create(getContext(),sQuinticInterpolator);
+        }
+        @Override
+        public void run() {
+            final ScrollerCompat scroller = mScroller;
+            if (scroller.computeScrollOffset()){
+                final int x = mScroller.getCurrX();
+                final int y = mScroller.getCurrY();
+                int dx = x - mLastFlingX;
+                int dy = y - mLastFlingY;
+                mLastFlingX = x;
+                mLastFlingY = y;
+                scroll(dx,0);
+                if (lineRender.canScroll() && !scroller.isFinished()){
+                    postOnAnimation();
+                }
+            }
+        }
+
+        void postOnAnimation(){
+            removeCallbacks(this);
+            ViewCompat.postOnAnimation(LineChart.this,this);
+        }
+        public void fling(int velocityX, int velocityY){
+            setScrollState(SCROLL_STATE_SETTLING);
+            mLastFlingX = mLastFlingY = 0;
+            mScroller.fling(0, 0, velocityX, velocityY,
+                    Integer.MIN_VALUE, Integer.MAX_VALUE, Integer.MIN_VALUE, Integer.MAX_VALUE);
+            postOnAnimation();
+        }
+
+        public void stop(){
+            removeCallbacks(this);
+            mScroller.abortAnimation();
+        }
+
+
+    }
+    private void resetTouch() {
+        if (mVelocityTracker != null) {
+            mVelocityTracker.clear();
+        }
+    }
+    public boolean fling(int velocityX, int velocityY) {
+
+        if (Math.abs(velocityX) < mMinFlingVelocity) {
+            velocityX = 0;
+        }
+        if (Math.abs(velocityY) < mMinFlingVelocity) {
+            velocityY = 0;
+        }
+        if (velocityX == 0 && velocityY == 0) {
+            // If we don't have any velocity, return false
+            return false;
+        }
+
+        velocityX = Math.max(-mMaxFlingVelocity, Math.min(velocityX, mMaxFlingVelocity));
+        velocityY = Math.max(-mMaxFlingVelocity, Math.min(velocityY, mMaxFlingVelocity));
+        mViewFlinger.fling(velocityX, velocityY);
+        return true;
+    }
+
+
+    public void scroll(float dx,float dy){
+        Log.i(TAG, "scroll: --------"+dx +"---"+dy);
+        lineRender.refreshTouchMatrix(dx,dy);
+        invalidate();
+    }
+
+    private int mScrollState = SCROLL_STATE_IDLE;
+    private static final int INVALID_POINTER = -1;
+    public int getScrollState() {
+        return mScrollState;
+    }
+
+    void setScrollState(int state) {
+        if (state == mScrollState) {
+            return;
+        }
+
+        mScrollState = state;
+        if (state != SCROLL_STATE_SETTLING) {
+            stopScrollersInternal();
+        }
+    }
+    private void stopScrollersInternal() {
+        mViewFlinger.stop();
     }
 }
--- app/src/main/java/com/finance/recyclerviewdemo/kchart/LineRender.java
+++ app/src/main/java/com/finance/recyclerviewdemo/kchart/LineRender.java
@@ -10,21 +10,30 @@ import android.util.Log;
 import com.finance.recyclerviewdemo.kchart.bean.NewEntry;
 import com.finance.recyclerviewdemo.kchart.bean.NewEntryData;
 
+import java.text.DecimalFormat;
+import java.util.Arrays;
+
 /**
  * Created by Jackie on 2018/9/19.
  */
 public class LineRender {
-    protected Paint mPaint;
+    protected Paint mPaint,mLabelPaint;
     private NewEntryData data;
     private RectF contentRectF;
-    protected float mBarSpace = 0.8f;
+    protected float mBarSpace = 0.4f;
     // contain 2 points to draw a rect.
     protected float[] bodyBuffer = new float[4];
+    private float[] calcTemp = new float[]{0, 0};
     protected float maxTouchOffset, minTouchOffset;
+    private float defaultTextSize;
     public LineRender(){
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG); //看锯齿
         mPaint.setStyle(Paint.Style.FILL);
         mPaint.setStrokeWidth(1);
+
+        mLabelPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mLabelPaint.setColor(Color.BLACK);
+
     }
     /**
      * matrix to map the values to the screen pixels
@@ -34,69 +43,175 @@ public class LineRender {
 
     //和滚动相关的矩阵
     protected Matrix mMatrixTouch = new Matrix();
+    /**
+     * matrix to map the chart offset
+     * 映射图表偏移的矩阵
+     */
+    protected Matrix mMatrixOffset = new Matrix();
     private static final String TAG = "LineRender";
+    private Paint.FontMetrics fontMetrics = new Paint.FontMetrics();
     public void setData(NewEntryData data){
         this.data = data;
-        prepareMatrixTouch(5);
+        prepareMatrixTouch(visibleCount);
         prepareMatrixValue(data.mYMax,0);  //就按120 计算高度
+        //FIXME：变换后再进行平移，比较准确
+        prepareMatrixOffset(contentRectF.left,contentRectF.top);
     }
 
 
     //绘制所有
     public void render(Canvas canvas){
-
-//        calc();
+        calc();
+        renderLabels(canvas);
         Log.i(TAG, "render: ---："+contentRectF.left+"   top:"+contentRectF.top+"    right:"+contentRectF.right+"    bottom:"+contentRectF.bottom);
+        canvas.save();
         canvas.clipRect(contentRectF.left,contentRectF.top,contentRectF.right,contentRectF.bottom);
         mPaint.setColor(Color.RED);
+        Log.i(TAG, "render: ----"+ data.entries.size());
 //        canvas.drawCircle(100,100,20f,mPaint);
         for (int i = 0;i<data.entries.size();i++){
+            if (i == 0){
+                mPaint.setColor(Color.BLUE);
+            }else {
+                mPaint.setColor(Color.RED);
+            }
 //            Log.i(TAG, "render: "+data.entries.size());
             NewEntry entry = data.entries.get(i);
 
-            bodyBuffer[0] = i+1 - mBarSpace;
+            bodyBuffer[0] = entry.month - mBarSpace;
             bodyBuffer[1] = entry.money;
-            bodyBuffer[2] = i +mBarSpace;
+            bodyBuffer[2] = entry.month;
             bodyBuffer[3] = 0;
 //            Log.i(TAG, "render: "+bodyBuffer[0]+"-----"+bodyBuffer[1]+"----"+bodyBuffer[2]+"-----"+bodyBuffer[3]);
             mapPoints(bodyBuffer);
-//            Log.i(TAG, "render:0000000000: "+bodyBuffer[0]+"-----"+bodyBuffer[1]+"----"+bodyBuffer[2]+"-----"+bodyBuffer[3]+"---------"+i);
+            Log.i(TAG, "render:0000000000: "+bodyBuffer[0]+"-----"+bodyBuffer[1]+"----"+bodyBuffer[2]+"-----"+bodyBuffer[3]+"---------"+i);
             canvas.drawRect(bodyBuffer[0],bodyBuffer[1],bodyBuffer[2],bodyBuffer[3],mPaint);
 
         }
+        canvas.restore();
 
+    }
+    protected DecimalFormat decimalFormatter = new DecimalFormat("0.00");
+    /**
+     * Draw x and y labels.
+     */
+    protected void renderLabels(Canvas canvas) {
+        // DRAW Y LABELS
+        mLabelPaint.setTextAlign(Paint.Align.RIGHT);
+
+        //draw max y value
+        calcTemp[1] = contentRectF.top;
+        revertMapPoints(calcTemp);
+        String value = decimalFormatter.format(calcTemp[1]);
+        mLabelPaint.setTextSize(10);
+        mLabelPaint.setTextSize(contentRectF.left * 9 / mLabelPaint.measureText(value));
+        mLabelPaint.getFontMetrics(fontMetrics);
+        canvas.drawText(
+                value,
+                contentRectF.left * 9 / 10,
+                contentRectF.top - fontMetrics.top - fontMetrics.bottom,
+                mLabelPaint);
+
+        // draw min y value
+        calcTemp[1] = contentRectF.bottom;
+        revertMapPoints(calcTemp);
+        value = decimalFormatter.format(calcTemp[1]);
+        mLabelPaint.setTextSize(10);
+        mLabelPaint.setTextSize(contentRectF.left * 10 / mLabelPaint.measureText(value));
+        mLabelPaint.getFontMetrics(fontMetrics);
+        canvas.drawText(
+                value,
+                contentRectF.left * 9 / 10,
+                contentRectF.bottom - fontMetrics.bottom,
+                mLabelPaint);
+
+        calcTemp[1] = contentRectF.height() / 3 + contentRectF.top;
+        revertMapPoints(calcTemp);
+        value = decimalFormatter.format(calcTemp[1]);
+        mLabelPaint.setTextSize(10);
+        mLabelPaint.setTextSize(contentRectF.left * 10 / mLabelPaint.measureText(value));
+        mLabelPaint.getFontMetrics(fontMetrics);
+        canvas.drawText(
+                value,
+                contentRectF.left * 9 / 10,
+                contentRectF.height() / 3 + contentRectF.top + fontMetrics.bottom,
+                mLabelPaint);
+
+        calcTemp[1] = contentRectF.height() * 2 / 3 + contentRectF.top;
+        revertMapPoints(calcTemp);
+        value = decimalFormatter.format(calcTemp[1]);
+        mLabelPaint.setTextSize(10);
+        mLabelPaint.setTextSize(contentRectF.left * 10 / mLabelPaint.measureText(value));
+        mLabelPaint.getFontMetrics(fontMetrics);
+        canvas.drawText(
+                value,
+                contentRectF.left * 9 / 10,
+                contentRectF.height() * 2 / 3 + contentRectF.top + fontMetrics.bottom,
+                mLabelPaint);
+
+        canvas.save();
+        canvas.clipRect(contentRectF.left, contentRectF.bottom, contentRectF.right, contentRectF.bottom + 30);
+        mLabelPaint.setTextAlign(Paint.Align.CENTER);
+        mLabelPaint.setTextSize(defaultTextSize);
+        mLabelPaint.getFontMetrics(fontMetrics);
+        for (int i = visibleXMin; i < 12; i++) {
+            if (i % 2 == 0) {
+                calcTemp[0] = i + 0.5f;
+                mapPoints(calcTemp);
+                canvas.drawText(
+                        i + "",
+                        calcTemp[0],
+                        contentRectF.bottom + 15,
+                        mLabelPaint);
+//                canvas.drawLine(calcTemp[0], contentRectF.top, calcTemp[0], contentRectF.bottom, mLabelPaint);
+            }
+        }
+        canvas.restore();
+    }
+
+    protected float[] matrixValues = new float[9];
+    //刷新矩阵
+    public void refreshTouchMatrix(float dx,float dy){
+        isOnBorder = true;
+        mMatrixTouch.getValues(matrixValues);
+        matrixValues[Matrix.MTRANS_X] += -dx;
+        matrixValues[Matrix.MTRANS_Y] += dy;
+
+        if (matrixValues[Matrix.MTRANS_X] < -maxTouchOffset) {
+            matrixValues[Matrix.MTRANS_X] = -maxTouchOffset;
+            isOnBorder = false;
+        }
+        if (matrixValues[Matrix.MTRANS_X] > 0) {
+            matrixValues[Matrix.MTRANS_X] = 0;
+            isOnBorder = false;
+        }
+
+        mMatrixTouch.setValues(matrixValues);
+
+
+    }
+    private boolean isOnBorder = true;
+    public boolean canScroll() {
+        return isOnBorder;
     }
 
 
-    private void resetScrollRange(float scaleX){
-        minTouchOffset = 0;
-        //最大的可平移距离，因为已经有了一屏显示了，所以要减 1，可移动的距离
-        maxTouchOffset = contentRectF.width() * (scaleX - 1f);
-
-    }
-
-
-
-
-    protected void calc(){
-        float[] pixels = new float[]{
-                contentRectF.right, 0
-        };
-        revertMapPoints(pixels);
-
-        prepareMatrixValue(data.mYMax - data.mYMin,0);
-
-    }
-
+    //坐标点的映射
     protected void mapPoints(float[] pts){
         mMatrixValue.mapPoints(pts);
         mMatrixTouch.mapPoints(pts);
+        mMatrixOffset.mapPoints(pts);
     }
 
     protected void revertMapPoints(float[] pixels){
         Matrix temp = new Matrix();
         // invert all matrices to convert back to the original value 反转所有矩阵以获取初始值
         mMatrixValue.invert(temp);//相当于求逆矩阵，tem就是逆矩阵
+        temp.mapPoints(pixels);
+        mMatrixTouch.invert(temp);
+        temp.mapPoints(pixels);
+        Log.i(TAG, "revertMapPoints: ---2---" + Arrays.toString(pixels));
+        mMatrixValue.invert(temp);
         temp.mapPoints(pixels);
     }
 
@@ -109,11 +224,18 @@ public class LineRender {
         float scaleY = contentRectF.height()/deltaY;
         mMatrixValue.reset();
         mMatrixValue.postScale(scaleX,-scaleY);
-        mMatrixValue.postTranslate(0,contentRectF.height() + contentRectF.top);
+        mMatrixValue.postTranslate(0,contentRectF.height());
     }
+
+    public void prepareMatrixOffset(float leftOffset,float topOffset){
+        mMatrixOffset.reset();
+        mMatrixOffset.postTranslate(leftOffset,topOffset);
+    }
+
     //区域
     public void setContentRect(RectF contentRect){
         this.contentRectF = contentRect;
+        defaultTextSize = contentRectF.top * 3 / 4;
     }
 
     public void prepareMatrixTouch(float visableCount){
@@ -121,12 +243,40 @@ public class LineRender {
         float scaleY = 1;
         mMatrixTouch.reset();
         mMatrixTouch.postScale(scaleX,scaleY);
-
+        resetScrollRange(scaleX);
         //把矩阵移动，向右移动，实现一开始显示最左边的数据，这里无需移动，因为一开始就在最左
+//        mMatrixTouch.postTranslate(-maxTouchOffset,0);
 
+    }
+    private void resetScrollRange(float scaleX){
+        minTouchOffset = 0;
+        //最大的可平移距离，因为已经有了一屏显示了，所以要减 1，可移动的距离
+        maxTouchOffset = contentRectF.width() * (scaleX - 1f);
 
     }
 
+    protected int visibleCount = 6;
+    protected int visibleXMin, visibleXMax;
+    protected void calc(){
+            // calc step 0: calc min&max x index
+            float[] pixels = new float[]{
+                    contentRectF.right, 0
+            };
+            revertMapPoints(pixels);
+            visibleXMin = (pixels[0] <= 0) ? 0 : (int) (pixels[0]);
+            visibleXMax = visibleXMin + visibleCount;// plus visibleCount+1 for smooth disappear both side.
+            Log.i(TAG, "calc: ========" + Arrays.toString(pixels));
+            if (visibleXMax > data.entries.size()) {
+                visibleXMax = data.entries.size();
+            }
+
+            // calc step 1: calc min&max y value
+            data.calcMinMax(visibleXMin, visibleXMax);
+            prepareMatrixValue(data.mYMax, 0);
+//            prepareMatrixBar(data.mMaxYVolume);
+
+
+    }
 
 
 }
